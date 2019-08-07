a92203a80fe66e72403c2161fe6ada3df9bef0a9
sundowndev <raphael@crvx.fr>
2019-08-07 16:10:38 +0200

    refactor: module interfaces

--- packages/module-interfaces/src/index.ts
+++ packages/module-interfaces/src/index.ts
@@ -1,9 +1,1 @@
 export type FuncTest = 42 | 2 | 5;
-
-export interface IMigrationOptions {
-  logs?: boolean;
-  logger?: any;
-  logIfLatest?: boolean;
-  collectionName?: string;
-  db: string;
-}
